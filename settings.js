4b9126f2f69e51dd46302afa58303e79d7525cbf
ryoromance <100901480+NayBot12@users.noreply.github.com>
2022-03-12 00:50:56 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,15 +22,15 @@ global.botname = "×፝֟͜×RyoDontCare×፝֟͜×"
 global.ownername= "RyoDontCare×፝֟͜×"
 global.myweb = "https://api-alphabot.herokuapp.com/"
 global.youtube = "https://youtube.com/c/Ryo Bot"
-global.github = "https://zeeoneofc.github.io/"
-global.ownernomer = "6282129353366"
-global.ownernomerr = "+6282129353366"
+global.github = "https://NayBot12.github.io/"
+global.ownernomer = "6281223918622"
+global.ownernomerr = "+6281223918622"
 global.thumbnail = "./image/lol.jpg"
 global.donasi = "./image/donasi.jpg"
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6281646884686","6282129353366"]//ganti agar fitur owner bisa di gunakan
 global.packname = '© RyoDONTCARE' //sticker wm
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm
+global.author = 'Di Buat Oleh RiyoOfc' //sticker wm
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
@@ -40,16 +40,16 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    sukses: '🤗Done, Oke Desu~',
+    sukses: '🎀DONE NGAB~',
     admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
     botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
-    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh RiyoOfc !',
     group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
     private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
 	bot: 'Fitur Khusus Pengguna Nomor Bot',
     errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
     wait: '⏳ Sedang Di Proses',
-	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh RiyoOfc!',
 	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
